d477bd86c8fa9f17d3a2ca05d0ae9e4110e1bcb2
sa235 <sa235@live.ru>
2018-10-27 14:28:33 +0500

    Add confoose2 new project

---
+++ source/Confoose2/Confoose2/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Confoose2.Areas.Identity.Data;
+using Confoose2.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Confoose2.Areas.Identity.IdentityHostingStartup))]
+namespace Confoose2.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<ConfooseContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("ConfooseContextConnection")));
+
+                services.AddDefaultIdentity<ConfooseUser>()
+                    .AddEntityFrameworkStores<ConfooseContext>();
+            });
+        }
+    }
+}
